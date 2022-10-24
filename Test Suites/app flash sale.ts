45d5fa7211e67e39704e43f470f0b043675e020f
Hendry6666 <bramismet70@gmail.com>
2022-10-25 03:38:29 +0700

    Katalon Test Shopping app

---
+++ Test Suites/app flash sale.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>app flash sale</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a2c669de-c6bb-4b0f-9cff-af3a12f151fb</testSuiteGuid>
+   <testCaseLink>
+      <guid>689e3ce8-0814-4b6d-86d4-fa224279aecf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shopping</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
